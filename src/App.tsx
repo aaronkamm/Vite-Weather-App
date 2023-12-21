3430d858aceb24450e4fe3044729ea9cb94dd611
aaron <aaronakamm@gmail.com>
2023-12-20 16:33:08 -0800

    Installed & configured React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,26 @@
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { useState } from "react";
+
 function App() {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false
+      }
+    }
+  }));
+
   return (
-    <>
-      <div>
-        Hello World -- Temporary placeholder
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <div className="prose">
+        <h1>Welcome to Weather App!</h1>
+        <div>
+          <p>Enter your zip code to retrieve the weather:</p>
+        </div>
       </div>
-      
-    </>
+    </QueryClientProvider>
   )
 }
 
